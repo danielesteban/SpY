ce3cdd778f9b813dde79a7aaae89d36aeade4c2e
Daniel Esteban Nombela <dani@gatunes.com>
2018-11-19 00:12:17 +0100

    Tweak default zoom for mobiles

--- src/engine/camera.js
+++ src/engine/camera.js
@@ -7,7 +7,7 @@ import {
 class Camera extends PerspectiveCamera {
   constructor() {
     super(60, 1, 0.01, 2048);
-    this.distance = 9;
+    this.distance = 6;
     this.offset = new Vector3(0, 1.5, 0);
     this.tilt = Math.PI * -0.5;
     this.pitch = Math.PI * 0.125;
