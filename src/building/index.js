1387efd4c1743f0cee6dfb9c9df3e9f3d132d511
Daniel Esteban Nombela <dani@gatunes.com>
2018-11-19 06:41:34 +0100

    Refactor building code to be data driven

---
+++ src/building/index.js
@@ -0,0 +1,64 @@
+import { Object3D } from 'three';
+import Dude from '@/actors/dude';
+import Hallway from './hallway';
+import Shaft from './shaft';
+
+class Building extends Object3D {
+  constructor({
+    dudes,
+    elevators,
+    floors,
+  }) {
+    super();
+    this.dudes = dudes.map(({
+      pallete,
+      position,
+    }) => {
+      const mesh = new Dude(pallete);
+      mesh.position.copy(position);
+      this.add(mesh);
+      return mesh;
+    });
+    this.elevators = elevators.map(({
+      floors,
+      origin,
+    }) => {
+      const shaft = new Shaft({ floors, origin });
+      this.add(shaft);
+      return {
+        floors,
+        origin,
+        shaft,
+      };
+    });
+    this.floors = floors.map((rooms, floor) => rooms.map(({ type }, room) => {
+      let isEdge = false;
+      if (room === 0) isEdge = 'left';
+      else if (room === rooms.length - 1) isEdge = 'right';
+      const hasElevator = (
+        this.elevators.findIndex(({
+          floors,
+          origin,
+        }) => (
+          origin.x === room - Math.floor(rooms.length / 2)
+          && origin.y <= floor
+          && origin.y + floors > floor
+        ))
+      ) !== -1;
+      let mesh;
+      switch (type) {
+        default:
+          mesh = new Hallway({
+            hasElevator,
+            isEdge,
+          });
+          break;
+      }
+      mesh.position.set(room * 4 - (Math.floor(rooms.length / 2) * 4), floor * 3, 0);
+      this.add(mesh);
+      return mesh;
+    }));
+  }
+}
+
+export default Building;
--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,7 +1,6 @@
 import { Vector3 } from 'three';
 import Dude from '@/actors/dude';
-import Hallway from '@/rooms/hallway';
-import Shaft from '@/rooms/shaft';
+import Building from '@/building';
 import Marker from '@/items/marker';
 
 export default ({ input, scene }) => {
@@ -25,61 +24,22 @@ export default ({ input, scene }) => {
   scene.camera.target = dude.position;
 
   /* Spawn test building */
-  const building = {
-    dudes: [...Array(4)].map(() => {
-      const dude = new Dude({
+  const building = new Building({
+    dudes: [...Array(4)].map(() => ({
+      pallete: {
         arms: 0x222222,
         eyes: 0x999999 * Math.random(),
         head: 0x999999 * Math.random(),
         legs: 0x222222,
         torso: 0x999999 * Math.random(),
-      });
-      dude.position.set(
+      },
+      position: new Vector3(
         Math.random() * 19 - 9.5,
         Math.floor(Math.random() * 3) * 3,
         0
-      );
-      return dude;
-    }),
-    elevators: [],
-    floors: [
-      [...Array(5)].map((v, i) => {
-        const hallway = new Hallway({
-          hasElevator: i === 1,
-          // eslint-disable-next-line no-nested-ternary
-          isEdge: i === 0 ? 'left' : (i === 4 ? 'right' : false),
-        });
-        hallway.position.set(i * 4 - (2 * 4), 0, 0);
-        return hallway;
-      }),
-      [...Array(5)].map((v, i) => {
-        const hallway = new Hallway({
-          hasElevator: i === 1 || i === 3,
-          // eslint-disable-next-line no-nested-ternary
-          isEdge: i === 0 ? 'left' : (i === 4 ? 'right' : false),
-        });
-        hallway.position.set(i * 4 - (2 * 4), 3, 0);
-        return hallway;
-      }),
-      [...Array(5)].map((v, i) => {
-        const hallway = new Hallway({
-          hasElevator: i === 3,
-          // eslint-disable-next-line no-nested-ternary
-          isEdge: i === 0 ? 'left' : (i === 4 ? 'right' : false),
-        });
-        hallway.position.set(i * 4 - (2 * 4), 6, 0);
-        return hallway;
-      }),
-      [...Array(5)].map((v, i) => {
-        const hallway = new Hallway({
-          // eslint-disable-next-line no-nested-ternary
-          isEdge: i === 0 ? 'left' : (i === 4 ? 'right' : false),
-        });
-        hallway.position.set(i * 4 - (2 * 4), 9, 0);
-        return hallway;
-      }),
-    ],
-    shafts: [
+      ),
+    })),
+    elevators: [
       {
         floors: 2,
         origin: { x: -1, y: 0 },
@@ -88,15 +48,15 @@ export default ({ input, scene }) => {
         floors: 2,
         origin: { x: 1, y: 1 },
       },
-    ].map((meta => new Shaft(meta))),
-  };
-
-  [
-    ...building.dudes,
-    ...building.elevators,
-    ...building.floors.reduce((floors, floor) => [...floors, ...floor], []),
-    ...building.shafts,
-  ].forEach(mesh => scene.root.add(mesh));
+    ],
+    floors: [
+      [...Array(5)].map(() => ({ type: 'hallway' })),
+      [...Array(5)].map(() => ({ type: 'hallway' })),
+      [...Array(5)].map(() => ({ type: 'hallway' })),
+      [...Array(5)].map(() => ({ type: 'hallway' })),
+    ],
+  });
+  scene.root.add(building);
 
   /* Animation loop */
   const floor = 1;
@@ -111,7 +71,7 @@ export default ({ input, scene }) => {
       const { raycaster } = pointer;
       raycaster.setFromCamera(pointer.normalized, camera);
       const hit = raycaster.intersectObjects([
-        ...building.elevators,
+        // ...building.elevators,
         ...building.floors[floor],
       ])[0];
       if (!hit) return;
