87472c042b084b7fd06df7e0b62607a3870d6f58
Daniel Esteban Nombela <dani@gatunes.com>
2018-11-22 00:48:33 +0100

    Unify the meshes style

--- src/building/doors.js
+++ src/building/doors.js
@@ -1,10 +1,13 @@
 import {
-  PlaneBufferGeometry,
+  BufferGeometry,
+  Color,
   DoubleSide,
   Mesh,
   MeshPhongMaterial,
   Object3D,
+  PlaneGeometry,
   Vector3,
+  VertexColors,
 } from 'three';
 
 class Doors extends Object3D {
@@ -12,14 +15,23 @@ class Doors extends Object3D {
     floor,
   }) {
     super();
-    const geometry = new PlaneBufferGeometry(0.5, 1, Doors.scale.x * 0.5, Doors.scale.y);
-    geometry.scale(Doors.scale.x, Doors.scale.y, Doors.scale.z);
+    const plane = new PlaneGeometry(0.5, 1, Doors.scale.x * 0.5, Doors.scale.y);
+    const color = new Color();
+    plane.faces.forEach((face, i) => {
+      if (i % 2 === 0) {
+        color.setHSL(Math.random(), 0.05, 0.25 + (Math.random() * 0.125));
+      }
+      face.color.copy(color);
+    });
+    plane.scale(Doors.scale.x, Doors.scale.y, Doors.scale.z);
+    const geometry = (new BufferGeometry()).fromGeometry(plane);
     for (let i = 0; i < 2; i += 1) {
       const door = new Mesh(
         geometry,
         new MeshPhongMaterial({
           color: Doors.color,
           side: DoubleSide,
+          vertexColors: VertexColors,
         })
       );
       door.position.set(
@@ -42,7 +54,7 @@ class Doors extends Object3D {
   }
 }
 
-Doors.color = 0x111111;
+Doors.color = 0x333333;
 Doors.scale = new Vector3(2, 3, 2);
 
 export default Doors;
--- src/building/elevator.js
+++ src/building/elevator.js
@@ -1,10 +1,12 @@
 import {
   BoxGeometry,
   BufferGeometry,
+  Color,
   DoubleSide,
   Mesh,
   MeshPhongMaterial,
   Vector3,
+  VertexColors,
 } from 'three';
 
 class Elevator extends Mesh {
@@ -12,16 +14,28 @@ class Elevator extends Mesh {
     floors,
     origin,
   }) {
-    const geometry = new BoxGeometry(0.99, 0.99, 0.99);
+    const geometry = new BoxGeometry(
+      0.99, 0.99, 0.99,
+      Elevator.scale.x, Elevator.scale.y, Elevator.scale.z
+    );
+    const color = new Color();
+    geometry.faces.forEach((face, i) => {
+      if (i % 2 === 0) {
+        color.setHSL(Math.random(), 0.05, 0.25 + (Math.random() * 0.125));
+      }
+      face.color.copy(color);
+    });
     geometry.translate(0, 0.5, 0);
-    geometry.faces.splice(8, 1);
-    geometry.faces.splice(8, 1);
+    for (let i = 0; i < 12; i += 1) {
+      geometry.faces.splice(40, 1);
+    }
     geometry.scale(Elevator.scale.x, Elevator.scale.y, Elevator.scale.z);
     super(
       (new BufferGeometry()).fromGeometry(geometry),
       new MeshPhongMaterial({
         color: Elevator.color,
         side: DoubleSide,
+        vertexColors: VertexColors,
       })
     );
     this.floors = floors;
@@ -36,7 +50,7 @@ class Elevator extends Mesh {
   }
 }
 
-Elevator.color = 0x330000;
+Elevator.color = 0x440000;
 Elevator.scale = new Vector3(2, 3, 2);
 
 export default Elevator;
