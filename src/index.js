94554255c881143704f75c1a11b40969c5440b7c
Daniel Esteban Nombela <dani@gatunes.com>
2018-11-22 02:26:00 +0100

    Wait for fonts to be loaded

--- src/index.js
+++ src/index.js
@@ -6,14 +6,49 @@ const mount = document.getElementById('mount');
 const input = new Input({ mount });
 const scene = new Scene({ mount });
 
-input.touches.once('end', () => {
-  document.getElementById('splash').style.display = 'none';
-  input.isEnabled = true;
-});
-
 let route = window.location.hash.split('/').slice(1)[0];
 if (!Scenes[route]) {
   window.location.hash = '/game';
   route = 'game';
 }
-Scenes[route]({ input, scene });
+window.addEventListener('hashchange', () => window.location.reload(), false);
+
+function onLoad() {
+  document.body.className = 'loaded';
+  Scenes[route]({ input, scene });
+  input.touches.once('end', () => {
+    document.getElementById('splash').style.display = 'none';
+    input.isEnabled = true;
+  });
+}
+
+function waitForFonts(fonts) {
+  let loaded = 0;
+  fonts.forEach(({ font, test }) => {
+    const fontTester = document.createElement('div');
+    fontTester.innerText = test;
+    fontTester.style.position = 'absolute';
+    fontTester.style.fontFamily = 'sans-serif';
+    fontTester.style.top = '-999999px';
+    document.body.appendChild(fontTester);
+    const { width: initialWidth } = fontTester.getBoundingClientRect();
+    fontTester.style.fontFamily = font;
+    const check = () => {
+      const { width } = fontTester.getBoundingClientRect();
+      if (width > initialWidth) {
+        document.body.removeChild(fontTester);
+        loaded += 1;
+        if (loaded === fonts.length) {
+          onLoad();
+        }
+      } else {
+        setTimeout(check, 10);
+      }
+    };
+    check();
+  });
+}
+
+waitForFonts([
+  { font: "'Print Char 21'", test: 'QW@HhsXJ' },
+]);
