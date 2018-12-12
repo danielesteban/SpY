be74dce0e84b7c53387ac962893c0dfea447a6a4
Daniel Esteban Nombela <dani@gatunes.com>
2018-12-12 03:44:44 +0100

    Editor toolbar styles

--- src/ui/editor.js
+++ src/ui/editor.js
@@ -3,21 +3,35 @@ import UI from '@/core/ui';
 class EditorUI extends UI {
   constructor() {
     super('toolbar');
-    this.buttons = ['Air', 'Floor', 'Wall'].map((label, tool) => {
-      const button = this.add('button');
-      button.innerText = label;
-      button.addEventListener('click', () => {
-        this.setTool(tool);
-      }, false);
-      return button;
-    });
-    this.setTool(1);
     this.color = this.add('input');
     this.color.type = 'color';
     this.color.addEventListener('change', ({ target: { value: color } }) => {
       this.setColor(color);
     }, false);
     this.setColor('#bebebe');
+    this.buttons = [
+      {
+        label: 'Floor',
+        tool: 1,
+      },
+      {
+        label: 'Wall',
+        tool: 2,
+      },
+      {
+        label: 'Erase',
+        tool: 0,
+      },
+    ].map(({ label, tool }) => {
+      const button = this.add('button');
+      button.innerText = label;
+      button.tool = tool;
+      button.addEventListener('click', () => {
+        this.setTool(tool);
+      }, false);
+      return button;
+    });
+    this.setTool(1);
     this.mount();
   }
 
@@ -31,8 +45,8 @@ class EditorUI extends UI {
 
   setTool(value) {
     const { buttons } = this;
-    buttons.forEach((button, tool) => {
-      button.className = tool === value ? 'active' : '';
+    buttons.forEach((button) => {
+      button.className = button.tool === value ? 'active' : '';
     });
     this.tool = value;
     if (this.onToolChange) {
